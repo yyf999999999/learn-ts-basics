2cfee00fcbf96324ea657453f0177eab50ee27e7
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2024-10-03 13:38:56 +0900

    prac追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-console.log("■ prac00.ts の実行");
-import "./prac00";
+//console.log("■ prac00.ts の実行");
+//import "./prac00";
 
 console.log("■ prac01.ts の実行");
 import "./prac01";
+
+//console.log("■ date.ts の実行");
+//import "./date";
--- src/prac01.ts
+++ src/prac01.ts
@@ -1,4 +1,9 @@
 export {};
 
-const userName: string = "Bob";
-console.log(`Hi, ${userName}.`);
+const todo = {
+  name: "TypeScriptの勉強", // name = "..." ではない点に要注意
+  priority: 3, // priority = "..." ではない点に要注意
+};
+console.log(`Todo 1 => ${todo.name}（優先度:${todo.priority})`);
+console.log(todo);
+console.log(JSON.stringify(todo, null, 2));
